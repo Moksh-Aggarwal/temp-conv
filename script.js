c300d35d32ab3c08a2bebb4c366cdbede911a2c7
Moksh aggarwal <95561331+Moksh-Aggarwal@users.noreply.github.com>
2023-09-02 23:39:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function convertTemperature(inputType) {
+    const celsiusInput = document.getElementById('celsius');
+    console.log(celsiusInput)
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    console.log(fahrenheitInput)
+    
+    if (inputType === 'celsius') {
+      const celsiusValue = celsiusInput.value;
+      if (celsiusValue !== '') {
+        const fahrenheitValue = (celsiusValue * 9/5) + 32;
+        fahrenheitInput.value = fahrenheitValue.toFixed(2);
+      } else {
+        fahrenheitInput.value = '';
+      }
+    } else if (inputType === 'fahrenheit') {
+      const fahrenheitValue = fahrenheitInput.value;
+      if (fahrenheitValue !== '') {
+        const celsiusValue = (fahrenheitValue - 32) * 5/9;
+        celsiusInput.value = celsiusValue.toFixed(2);
+      } else {
+        celsiusInput.value = '';
+      }
+    }
+  }
